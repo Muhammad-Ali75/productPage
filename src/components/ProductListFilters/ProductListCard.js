01ecfd4451fe7581861238c6c7f9cf1787926d20
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-06-14 22:05:44 +0500

    product List card

--- src/components/ProductListFilters/ProductListCard.js
+++ src/components/ProductListFilters/ProductListCard.js
@@ -1,32 +1,48 @@
 import usFlag from "../../assets/usaflag.svg";
-function ProductListCard() {
+function ProductListCard({ title = "title", isUsa = true, moq = 1000, lowPrice = 1, highPrice = 200 }) {
   return (
     <div
       style={{
         width: "100%",
         height: "10rem",
-        border: "1px solid black",
+        border: "1px solid #e2e8f0",
+        borderRadius: 5,
         marginBottom: 10,
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
         padding: "0.5rem",
+
       }}
     >
       <img
         src="https://tbh-production.s3.ap-southeast-1.amazonaws.com/Product/2034888965/Images/16066601421950233806.png"
         alt="productImg"
-        style={{ height: "auto", width: "auto", maxHeight: "100%" }}
+        style={{ height: "auto", width: "auto", maxHeight: "9rem" }}
       />
-      <div>
-        <p>title</p>
-        <div>
-          <img src={usFlag} alt="us-flag" />
-        </div>
-        <p>moq</p>
-        <p>price</p>
+      <div style={{ width: "100%", display: 'flex', flexDirection: "column", }}>
+        <p style={{ paddingBottom: 5 }}>{title || "TITLE"}</p>
+        {isUsa && <div style={{ flexDirection: "row", display: "flex", paddingBottom: 5 }}>
+          <img src={usFlag} alt="us-flag" style={{ height: "2rem", width: "2rem" }} />
+          <p style={{ fontSize: 12, paddingLeft: 10 }}>Stock in USA</p>
+        </div>}
+        <p style={{ fontSize: 14, color: "#ababab", paddingBottom: 5 }}>MOQ: {moq} Peices</p>
+        <h3 style={{ fontSize: 18, paddingBottom: 5 }}>$ {highPrice ? `${lowPrice} - $ ${highPrice}` : lowPrice} / Peices</h3>
 
-        <button>Add to cart</button>
+        <button style={{
+          width: "9rem",
+          height: "2rem",
+          fontSize: 16,
+          fontWeight: 400,
+          background: '#00b2c9',
+          color: "white",
+          borderRadius: 8,
+          border: "0px",
+          padding: 4,
+          paddingLeft: 24,
+          paddingRight: 24,
+
+        }}>Add to cart</button>
       </div>
     </div>
   );
