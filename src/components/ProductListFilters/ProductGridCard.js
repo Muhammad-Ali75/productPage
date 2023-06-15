9ddacd8eb3f8c264a48886e1de66882ba0cd5f8f
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-06-15 12:15:37 +0500

    adds product grid card

--- src/components/ProductListFilters/ProductGridCard.js
+++ src/components/ProductListFilters/ProductGridCard.js
@@ -1,16 +1,38 @@
-function ProductGridCard() {
+import usFlag from "../../assets/usaflag.svg";
+import "./styles/ProductGridCard.css";
+
+function ProductGridCard({
+  title,
+  isUsa = true,
+  moq = 1000,
+  lowPrice = 1,
+  highPrice = 200,
+}) {
   return (
-    <div
-      style={{
-        width: "33%",
-        minWidth: 200,
-        height: 100,
-        borderStyle: "solid",
-        borderWidth: 1,
-        borderColor: "black",
-      }}
-    >
-      grip card
+    <div className="gridCardContainer">
+      <img
+        src="https://tbh-production.s3.ap-southeast-1.amazonaws.com/Product/2034888965/Images/16066601421950233806.png"
+        alt="productImg"
+        className="imageStyle"
+      />
+      <div className="infoContainer">
+        {isUsa && (
+          <div className="usflagContainer">
+            <img src={usFlag} alt="us-flag" className="usFlagStyle" />
+            <p className="usFlagText">Stock in USA</p>
+          </div>
+        )}
+        <p className="titleStyle">
+          {title ||
+            "TITLE lorem ips lorem ipsum dolor sit amet, consectetur adip lorem sit"}
+        </p>
+        <p className="moqText">MOQ: {moq} Peices</p>
+        <h3 className="priceText">
+          $ {highPrice ? `${lowPrice} - $ ${highPrice}` : lowPrice} / Peices
+        </h3>
+
+        <button>Add to cart</button>
+      </div>
     </div>
   );
 }
--- src/components/ProductListFilters/ProductListCard.js
+++ src/components/ProductListFilters/ProductListCard.js
@@ -1,5 +1,11 @@
 import usFlag from "../../assets/usaflag.svg";
-function ProductListCard({ title = "title", isUsa = true, moq = 1000, lowPrice = 1, highPrice = 200 }) {
+function ProductListCard({
+  title = "title",
+  isUsa = true,
+  moq = 1000,
+  lowPrice = 1,
+  highPrice = 200,
+}) {
   return (
     <div
       style={{
@@ -12,7 +18,6 @@ function ProductListCard({ title = "title", isUsa = true, moq = 1000, lowPrice =
         flexDirection: "row",
         alignItems: "center",
         padding: "0.5rem",
-
       }}
     >
       <img
@@ -20,29 +25,46 @@ function ProductListCard({ title = "title", isUsa = true, moq = 1000, lowPrice =
         alt="productImg"
         style={{ height: "auto", width: "auto", maxHeight: "9rem" }}
       />
-      <div style={{ width: "100%", display: 'flex', flexDirection: "column", }}>
+      <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
         <p style={{ paddingBottom: 5 }}>{title || "TITLE"}</p>
-        {isUsa && <div style={{ flexDirection: "row", display: "flex", paddingBottom: 5 }}>
-          <img src={usFlag} alt="us-flag" style={{ height: "2rem", width: "2rem" }} />
-          <p style={{ fontSize: 12, paddingLeft: 10 }}>Stock in USA</p>
-        </div>}
-        <p style={{ fontSize: 14, color: "#ababab", paddingBottom: 5 }}>MOQ: {moq} Peices</p>
-        <h3 style={{ fontSize: 18, paddingBottom: 5 }}>$ {highPrice ? `${lowPrice} - $ ${highPrice}` : lowPrice} / Peices</h3>
+        {isUsa && (
+          <div
+            style={{ flexDirection: "row", display: "flex", paddingBottom: 5 }}
+          >
+            <img
+              src={usFlag}
+              alt="us-flag"
+              style={{ height: "2rem", width: "2rem" }}
+            />
+            <p style={{ fontSize: 12, paddingLeft: 10, paddingTop: 5 }}>
+              Stock in USA
+            </p>
+          </div>
+        )}
+        <p style={{ fontSize: 14, color: "#ababab", paddingBottom: 5 }}>
+          MOQ: {moq} Peices
+        </p>
+        <h3 style={{ fontSize: 18, paddingBottom: 5 }}>
+          $ {highPrice ? `${lowPrice} - $ ${highPrice}` : lowPrice} / Peices
+        </h3>
 
-        <button style={{
-          width: "9rem",
-          height: "2rem",
-          fontSize: 16,
-          fontWeight: 400,
-          background: '#00b2c9',
-          color: "white",
-          borderRadius: 8,
-          border: "0px",
-          padding: 4,
-          paddingLeft: 24,
-          paddingRight: 24,
-
-        }}>Add to cart</button>
+        <button
+          style={{
+            width: "9rem",
+            height: "2rem",
+            fontSize: 16,
+            fontWeight: 400,
+            background: "#00b2c9",
+            color: "white",
+            borderRadius: 8,
+            border: "0px",
+            padding: 4,
+            paddingLeft: 24,
+            paddingRight: 24,
+          }}
+        >
+          Add to cart
+        </button>
       </div>
     </div>
   );
