c2ddddf65abdcc4600493e21adf8bf8112092ed7
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-06-19 11:52:40 +0500

    adds responsive filter section

--- src/components/ProductListFilters/ProductListFilters.js
+++ src/components/ProductListFilters/ProductListFilters.js
@@ -1,11 +1,25 @@
-import "./styles/ProductListFilters.css"
+import "./styles/ProductListFilters.css";
 import FiltersSection from "./FilterSection";
 import ProductSection from "./ProductSection";
+import { useState } from "react";
 
 function ProductListFilters() {
+  const [dropdown, setDropdown] = useState(false);
+
+  function toggleDropdown() {
+    setDropdown(!dropdown);
+  }
+
   return (
     <div className="product-list-filter-container">
-      {true && <FiltersSection />}
+      <div className="desktop-filters">{true && <FiltersSection />}</div>
+
+      <div className="mobile-filters">
+        <button onClick={toggleDropdown}>
+          {dropdown ? "Close Filters" : "Show Filters"}
+        </button>
+        {dropdown && <FiltersSection />}
+      </div>
       <ProductSection />
     </div>
   );
