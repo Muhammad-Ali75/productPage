e91a03404b8218479d1a68b7c618e42d95c299b5
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-06-21 11:14:57 +0500

    removed comments

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { Provider as PleProvider } from "./context/ProductListEnhancement.js";
 import "./styles/index.css";
 import App from "./App";
 
-// import { Context as UserContext } from "../Context/UserContext";
-// const { addPost, addProduct } = useContext(UserContext);const { state } = useContext(UserContext);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
